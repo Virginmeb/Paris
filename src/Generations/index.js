c806de1ccfca587bbe217d49cb41c95e47c320ce
kyrie pondaag <kyriepondaag21@gmail.com>
2023-06-20 21:28:37 +0800

    commit

---
+++ src/Generations/index.js
@@ -0,0 +1,18 @@
+// Generations.js
+import React from 'react';
+
+function Generations() {
+  return (
+    <div>
+      <h1>Generations</h1>
+      <ul>
+        <li>Gen 1</li>
+        <li>Gen 2</li>
+        <li>Gen 3</li>
+        <li>Gen 4</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Generations;

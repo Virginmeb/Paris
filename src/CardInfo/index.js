5b86eca852f264e058b0bd8a14c208566b6641ba
VirgilWeol <virgilweol25@gmail.com>
2023-06-21 00:09:03 +0800

    commit

---
+++ src/CardInfo/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Evolution from './Evolution';
+import TypeEffect from './TypeEffect';
+import Logo from './Logo';
+
+function CardInfo() {
+  return (
+    <div>
+      <Evolution />
+      <TypeEffect />
+      <Logo />
+    </div>
+  );
+}
+
+export default CardInfo;
+
